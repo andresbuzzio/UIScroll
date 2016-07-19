f76ed7d95966ab0be9ce8aaab6592016356b6d29
Andres Buzzio <andresbuzzio@gmail.com>
2016-07-19 12:37:14 +0200

    Update uiscroll.js

--- JS/uiscroll.js
+++ JS/uiscroll.js
@@ -1,8 +1,23 @@
 $(window).scroll(function() {
     var height = $(window).scrollTop();
 
-    if(height  > 200) {
+    if(height  > 100) {
         $('.box1').css("visibility","visible");
-        $('.box1').addClass('magictime slideLeftRetourn');
+        $('.box1').addClass('magictime swashIn');
+    }
+
+    if(height  > 200) {
+        $('.box2').css("visibility","visible");
+        $('.box2').addClass('magictime swashIn');
+    }
+
+    if(height  > 350) {
+        $('.box3').css("visibility","visible");
+        $('.box3').addClass('magictime perspectiveRightRetourn');
+    }
+
+    if(height  > 500) {
+        $('.box4').css("visibility","visible");
+        $('.box4').addClass('magictime perspectiveLeftRetourn');
     }
 });
